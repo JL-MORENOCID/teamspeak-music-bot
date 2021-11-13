662a118fa0f2dcecab96c0428bdee3c21b81b34b
JMC <jl.morenocid@gmail.com>
2021-11-13 14:18:04 +0100

    Added server and client sockets using SOCKET.IO

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import getData from "./services/getData"
-import { TeamSpeak, QueryProtocol } from "ts3-nodejs-library"
+import { readFileSync } from "fs"
+import { createServer } from "https"
+import { Server } from "socket.io"
 import { SERVER_QUERY_USER, SERVER_QUERY_PASS } from "./services/settings"
-
-//create a new connection
-TeamSpeak.connect({
-  host: "blueclouds.es",
-  protocol: QueryProtocol.RAW, //optional
-  queryport: 10081, //optional
-  serverport: 9987,
-  username: SERVER_QUERY_USER,
-  password: SERVER_QUERY_PASS,
-  nickname: "NodeJS Query Framework"
-}).then(async teamspeak => {
-  const clients = await teamspeak.clientList({ clientType: 0 })
-  clients.forEach(client => {
-    console.log("Sending 'Hello!' Message to", client.nickname)
-    client.message("Hello! PREPARA EL CULO")
-    
-  })
-}).catch(e => {
-  console.log("Catched an error!")
-  console.error(e)
-})
 
 // Make a call
 getData({command: "channellist"}, {"-topic": '', "-info": ''})
   .then(val => console.log(val))
+
+// HTTPS Server
+const httpServer = createServer({
+  key: readFileSync("src/certs/key.pem"),
+  cert: readFileSync("src/certs/cert.pem")
+});
+
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  console.log("Connected")
+});
+httpServer.listen(3000);
 
 /*
 listen(3000,() => {
---
+++ src/Client.js
@@ -0,0 +1,5 @@
+import { io } from "socket.io-client"
+
+// Client socket
+const socket = io() // Abre conexión
+
